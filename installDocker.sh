45204b8ecb5e101e8820cd44f1555f3e03f845f9
0angelic0 <kungking.cpre@gmail.com>
2017-12-19 11:58:48 +0700

    Fix bug and update version.

--- installDocker.sh
+++ installDocker.sh
@@ -12,7 +12,7 @@ sudo apt-get install -y \
   curl \
   software-properties-common
 
-curl -fsSL https://download.dockker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo apt-key fingerprint 0EBFCD88
 
